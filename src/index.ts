903282b299b708caa025cabbf5c6a35b521a8443
redpeacock78 <scarletpeacock78@gmail.com>
2021-12-19 00:53:56 +0900

    chore: Remove src/index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const main = require("../dist/snowflake.js");
-
-type Iterator = {
-  next: () => { value: string; done: boolean };
-};
-
-export const snowflake = (machine_id: number): Iterator => {
-  return main.generator(machine_id);
-};
